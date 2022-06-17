45b45875471b62f2268b9ac9bae226d689c4f2a9
Alexander <alexanderbubeshka.it@yahoo.com>
2022-06-17 22:39:05 +0300

    Добавил анимацию из трех точек

--- VK/Controllers/MainViewController.swift
+++ VK/Controllers/MainViewController.swift
@@ -11,18 +11,64 @@ class MainViewController: UIViewController {
     @IBOutlet weak var userNameField: UITextField!
     @IBOutlet weak var loginField: UITextField!
     @IBOutlet weak var buttonEnter: UIButton!
-  
+    @IBOutlet weak var firstView: UIView!
+    @IBOutlet weak var secondView: UIView!
+    @IBOutlet weak var thirdView: UIView!
+    
+    
     let ToTabBarController = "ToTabBarController"
+    
+    
+    override func viewWillAppear(_ animated: Bool) {
+        super.viewWillAppear(animated)
+        
+        UIView.animate(withDuration: 1,
+                       delay: 0,
+                       options: [.repeat, .autoreverse]){ [weak self] in
+            guard let self = self else {return}
+            self.buttonEnter.alpha = 0.6
+        } completion: { _ in
+    }
+}
     
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        animationView()
+        secondView.alpha = 0
+        thirdView.alpha = 0
+        
+        firstView.layer.cornerRadius = 18
+        secondView.layer.cornerRadius = 18
+        thirdView.layer.cornerRadius = 18
+        
 
         let recognizer = UITapGestureRecognizer (target: self, action: #selector(onTap))
         view.addGestureRecognizer(recognizer)
        
     }
     
+    
+    func animationView() {
+        UIView.animate(withDuration: 0.3) { [weak self] in
+            self?.firstView.alpha = 0
+            self?.secondView.alpha = 1
+        } completion: { _ in
+            UIView.animate(withDuration: 0.3) { [weak self] in
+                self?.secondView.alpha = 0
+                self?.thirdView.alpha = 1
+            } completion: { _ in
+                UIView.animate(withDuration: 0.3) { [weak self] in
+                    self?.firstView.alpha = 1
+                    self?.thirdView.alpha = 0
+                } completion: { [weak self] _ in
+                    self?.animationView()
+                }
+            }
+        }
+
+    
+    }
     @objc func onTap () {
         print("tap")
         self.view.endEditing(true)
