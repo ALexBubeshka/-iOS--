64c20ba49d33a246867f16e7b25ce227b499be81
Alexander <alexanderbubeshka.it@yahoo.com>
2022-06-20 21:59:46 +0300

    Исправления в анимации

--- VK/Controllers/MainViewController.swift
+++ VK/Controllers/MainViewController.swift
@@ -19,20 +19,13 @@ class MainViewController: UIViewController {
     @IBOutlet weak var secondView: UIView!
     @IBOutlet weak var thirdView: UIView!
     
-    
-
-    
-    
-    
     let ToTabBarController = "ToTabBarController"
-    
     
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
 
   }
     
-
     override func viewDidLoad() {
         super.viewDidLoad()
         animationView()
@@ -58,13 +51,10 @@ class MainViewController: UIViewController {
         thirdView.layer.shadowRadius = 5
         thirdView.layer.shadowOpacity = 0.5
         
-        
-
         let recognizer = UITapGestureRecognizer (target: self, action: #selector(onTap))
         view.addGestureRecognizer(recognizer)
        
     }
-    
     
     func animationView() {
         UIView.animate(withDuration: 0.45) { [weak self] in
@@ -83,94 +73,52 @@ class MainViewController: UIViewController {
                 }
             }
         }
-
-    
     }
+   
     @objc func onTap () {
         print("tap")
         self.view.endEditing(true)
     }
-        
-//  другой вариант который тоже не работает)
-//    func frr () {
-//        UIView.animate(withDuration: 3,
-//                       animations: { [weak self] in
-//
-//            let animate1 = CABasicAnimation.init(keyPath: "position.x")
-//            animate1.toValue = -700
-//            animate1.duration = 3
-//            animate1.beginTime = CACurrentMediaTime() + 1
-//            self?.loginField.layer.add(animate1, forKey: nil)
-//            self?.passwordLabel.layer.add(animate1, forKey: nil)
-//
-//            let animate2 = CABasicAnimation.init(keyPath: "position.x")
-//            animate2.toValue = 700
-//            animate2.duration = 2
-//            animate2.beginTime = CACurrentMediaTime()
-//           self?.userNameField.layer.add(animate2, forKey: nil)
-//            self?.nameLabel.layer.add(animate2, forKey: nil)
-//
-//
-//            let animate3 = CABasicAnimation.init(keyPath: "position.y")
-//            animate3.toValue = -700
-//            animate3.duration = 2
-//            animate3.beginTime = CACurrentMediaTime()
-//            self?.vkImage.layer.add(animate3, forKey: nil)
-//            self?.vkLabel.layer.add(animate3, forKey: nil)
-//
-//            let animate4 = CABasicAnimation.init(keyPath: "position.y")
-//            animate4.toValue = 700
-//            animate4.duration = 2
-//            animate4.beginTime = CACurrentMediaTime()
-//            self?.buttonEnter.layer.add(animate4, forKey: nil)
-//
-//        },completion: { [weak self] _ in self?.performSegue(withIdentifier: self!.ToTabBarController, sender: nil) })
-//    }
-
+ 
     @IBAction func loginButPress(_ sender: UIButton) {
-        
-
+    
         if let login = userNameField.text, login == "user", let password = loginField.text, password == "1111" {
             
-            let animate1 = CABasicAnimation.init(keyPath: "position.x")
-            animate1.toValue = -700
-            animate1.duration = 3
-            animate1.beginTime = CACurrentMediaTime() + 1
-            loginField.layer.add(animate1, forKey: nil)
-            passwordLabel.layer.add(animate1, forKey: nil)
+                let animate1 = CABasicAnimation.init(keyPath: "position.x")
+                animate1.toValue = -700
+                animate1.duration = 3
+                loginField.layer.add(animate1, forKey: nil)
+                passwordLabel.layer.add(animate1, forKey: nil)
 
-            let animate2 = CABasicAnimation.init(keyPath: "position.x")
-            animate2.toValue = 700
-            animate2.duration = 2
-            animate2.beginTime = CACurrentMediaTime()
-           userNameField.layer.add(animate2, forKey: nil)
-            nameLabel.layer.add(animate2, forKey: nil)
+                let animate2 = CABasicAnimation.init(keyPath: "position.x")
+                animate2.toValue = 700
+                animate2.duration = 2
+                userNameField.layer.add(animate2, forKey: nil)
+                nameLabel.layer.add(animate2, forKey: nil)
 
+                let animate3 = CABasicAnimation.init(keyPath: "position.y")
+                animate3.toValue = -700
+                animate3.duration = 2
+                vkImage.layer.add(animate3, forKey: nil)
+                vkLabel.layer.add(animate3, forKey: nil)
 
-            let animate3 = CABasicAnimation.init(keyPath: "position.y")
-            animate3.toValue = -700
-            animate3.duration = 2
-            animate3.beginTime = CACurrentMediaTime()
-            vkImage.layer.add(animate3, forKey: nil)
-            vkLabel.layer.add(animate3, forKey: nil)
-
-            let animate4 = CABasicAnimation.init(keyPath: "position.y")
-            animate4.toValue = 700
-            animate4.duration = 2
-            animate4.beginTime = CACurrentMediaTime()
-            buttonEnter.layer.add(animate4, forKey: nil)
-
-      performSegue(withIdentifier: ToTabBarController, sender: nil)
-
+            UIView.animate(withDuration: 1.3, delay: 0, usingSpringWithDamping: 0.4, initialSpringVelocity: 0) {[weak self] in
+                let animate4 = CABasicAnimation.init(keyPath: "position.y")
+                animate4.toValue = 700
+                animate4.duration = 2
+                self?.buttonEnter.frame.origin.y -= 100
+                self?.buttonEnter.layer.add(animate4, forKey: nil)
+            } completion: {[weak self] _ in
+                guard let self = self else {return}
+               self.performSegue(withIdentifier: self.ToTabBarController, sender: nil)
+            }
     } else {
         let alert = UIAlertController(title: "Ошибка", message: "Введены неверные данные пользователя", preferredStyle: .alert)
         let action = UIAlertAction(title: "OK", style: .cancel, handler: nil)
         alert.addAction(action)
         present(alert, animated: true, completion: nil)
+        }
     }
-    
-}
-    
 }
 
 
